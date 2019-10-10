f90b2d4740edbef3674f2c1fd9fab4795b13a403
nopost <nopost@qq.com>
2019-10-10 21:46:13 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,15 @@ if [ ! -z "$HTTP_PROXY" ]; then
   export http_proxy=$HTTP_PROXY
   export https_proxy=$HTTP_PROXY
   export ftp_proxy=$HTTP_PROXY
+  export no_proxy="localhost, 127.0.0.1, ::1"
+cat > /etc/profile.d/http_proxy_profile.sh <<EOF
+    export proxy="$HTTP_PROXY"
+    export http_proxy=$proxy
+    export https_proxy=$proxy
+    export ftp_proxy=$proxy
+    export no_proxy="localhost, 127.0.0.1, ::1"
+EOF
+chmod +x /etc/profile.d/http_proxy_profile.sh || true
 fi
 
 # Disable Strict Host checking for non interactive git clones
