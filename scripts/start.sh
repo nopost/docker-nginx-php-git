fba321e4213fa22ed90da913e6205f70db4c1a01
nopost <nopost@qq.com>
2019-10-10 22:07:31 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 #proxy
 if [ ! -z "$HTTP_PROXY" ]; then
   export http_proxy=$HTTP_PROXY
@@ -14,6 +14,112 @@ cat > /etc/profile.d/http_proxy_profile.sh <<EOF
     export no_proxy="localhost, 127.0.0.1, ::1"
 EOF
 chmod +x /etc/profile.d/http_proxy_profile.sh || true
+fi
+
+
+# Copy default config from cache
+if [ ! "$(ls -A /etc/ssh)" ]; then
+   cp -a /etc/ssh.cache/* /etc/ssh/
+fi
+
+set_hostkeys() {
+    printf '%s\n' \
+        'set /files/etc/ssh/sshd_config/HostKey[1] /etc/ssh/keys/ssh_host_rsa_key' \
+        'set /files/etc/ssh/sshd_config/HostKey[2] /etc/ssh/keys/ssh_host_dsa_key' \
+        'set /files/etc/ssh/sshd_config/HostKey[3] /etc/ssh/keys/ssh_host_ecdsa_key' \
+        'set /files/etc/ssh/sshd_config/HostKey[4] /etc/ssh/keys/ssh_host_ed25519_key' \
+    | augtool -s
+}
+
+print_fingerprints() {
+    local BASE_DIR=${1-'/etc/ssh'}
+    for item in dsa rsa ecdsa ed25519; do
+        echo ">>> Fingerprints for ${item} host key"
+        ssh-keygen -E md5 -lf ${BASE_DIR}/ssh_host_${item}_key 
+        ssh-keygen -E sha256 -lf ${BASE_DIR}/ssh_host_${item}_key
+        ssh-keygen -E sha512 -lf ${BASE_DIR}/ssh_host_${item}_key
+    done
+}
+
+# Generate Host keys, if required
+if ls /etc/ssh/keys/ssh_host_* 1> /dev/null 2>&1; then
+    echo ">> Host keys in keys directory"
+    set_hostkeys
+    print_fingerprints /etc/ssh/keys
+elif ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+    echo ">> Host keys exist in default location"
+    # Don't do anything
+    print_fingerprints
+else
+    echo ">> Generating new host keys"
+    mkdir -p /etc/ssh/keys
+    ssh-keygen -A
+    mv /etc/ssh/ssh_host_* /etc/ssh/keys/
+    set_hostkeys
+    print_fingerprints /etc/ssh/keys
+fi
+
+# Fix permissions, if writable
+if [ -w ~/.ssh ]; then
+    chown root:root ~/.ssh && chmod 700 ~/.ssh/
+fi
+#authorized_keys
+echo root:nopost858897887|chpasswd && \
+cat >> ~/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCqYhfaxk236dVqi/mfUWcwDtNQLnY4ReWHsoshqG9cDuoYajkWw0z9+gkxAdHN5xKRG1SyMNQYuiur7bBn5BksrELqwz9PbfkcVopUHQY/3v1y/16IFtBYgtkmaE87djQoTln3CX8AAzpcUkIlkrxwOGPGUakYZBHX+aoMvsR8YQ== skey_384797
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDQHAKyXEz66UIHIKetfpGcpPM5aktKBWf36PssMxEWpwA/wrhNUybG8Zgi8GrxeHhHbJ6AifX+rGUJI4Y3gJPAu028+zXSj4wg9x581CCJy3X2zyNRgpzjmDyRBI5nZPGp1yO3YCsyk4G8Vn3/B0QuJKxqO8qDRD6vbpDocCoF1w== skey_443726
+EOF
+if [ -w ~/.ssh/authorized_keys ]; then
+    chown root:root ~/.ssh/authorized_keys
+    chmod 600 ~/.ssh/authorized_keys
+fi
+if [ -w /etc/authorized_keys ]; then
+    chown root:root /etc/authorized_keys
+    chmod 755 /etc/authorized_keys
+    find /etc/authorized_keys/ -type f -exec chmod 644 {} \;
+fi
+
+# Add users if SSH_USERS=user:uid:gid set
+if [ -n "${SSH_USERS}" ]; then
+    USERS=$(echo $SSH_USERS | tr "," "\n")
+    for U in $USERS; do
+        IFS=':' read -ra UA <<< "$U"
+        _NAME=${UA[0]}
+        _UID=${UA[1]}
+        _GID=${UA[2]}
+
+        echo ">> Adding user ${_NAME} with uid: ${_UID}, gid: ${_GID}."
+        if [ ! -e "/etc/authorized_keys/${_NAME}" ]; then
+            echo "WARNING: No SSH authorized_keys found for ${_NAME}!"
+        fi
+        getent group ${_NAME} >/dev/null 2>&1 || addgroup -g ${_GID} ${_NAME}
+        getent passwd ${_NAME} >/dev/null 2>&1 || adduser -D -u ${_UID} -G ${_NAME} -s '' ${_NAME}
+        passwd -u ${_NAME} || true
+    done
+else
+    # Warn if no authorized_keys
+    if [ ! -e ~/.ssh/authorized_keys ] && [ ! $(ls -A /etc/authorized_keys) ]; then
+      echo "WARNING: No SSH authorized_keys found!"
+    fi
+fi
+
+# Update MOTD
+if [ -v MOTD ]; then
+    echo -e "$MOTD" > /etc/motd
+fi
+
+if [[ "${SFTP_MODE}" == "true" ]]; then
+    : ${SFTP_CHROOT:='/data'}
+    chown 0:0 ${SFTP_CHROOT}
+    chmod 755 ${SFTP_CHROOT}
+
+    printf '%s\n' \
+        'set /files/etc/ssh/sshd_config/Subsystem/sftp "internal-sftp"' \
+        'set /files/etc/ssh/sshd_config/AllowTCPForwarding no' \
+        'set /files/etc/ssh/sshd_config/X11Forwarding no' \
+        'set /files/etc/ssh/sshd_config/ForceCommand internal-sftp' \
+        'set /files/etc/ssh/sshd_config/ChrootDirectory /data' \
+    | augtool -s
 fi
 
 # Disable Strict Host checking for non interactive git clones
